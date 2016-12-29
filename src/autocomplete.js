89198fca27032d14afee54b5d047db2500d3fd2c
Petr Chromec <mortal_flesh@seznam.cz>
2016-12-29 10:50:31 +0100

    Autocomplete - clear options on keyup

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -9,23 +9,38 @@ export default class Autocomplete {
     init() {
         /*
          * todo
-         * - start subscribing on keyup
+         * - (done) start subscribing on keyup
          * - onkeyup
-         *      - clear options
+         *      - (done) clear options
          *      - delay 300ms between keyups
          *      - after delay fetch response
          *      - show response in ac-options
          */
 
         Observable.fromEvent(this.getInput(), 'keyup')
-            .switchMap((keyup) => this.performRequest())
+            .switchMap((keyup) => this.keyupHandler())
             .subscribe((response) => this.writeOptions(response));
     }
 
+    /** @private */
     getInput() {
         return this.jQuery('.ac-input', `#${this.autocompleteId}`);
     }
 
+    /** @private */
+    keyupHandler() {
+        this.clearOptions();
+
+        return this.performRequest();
+    }
+
+    /** @private */
+    clearOptions() {
+        return this.jQuery('.ac-options', `#${this.autocompleteId}`)
+            .empty();
+    }
+
+    /** @private */
     performRequest() {
         return new Promise((resolve, reject) => {
             const timeout = Observable.interval(1000).take(1);
@@ -34,9 +49,8 @@ export default class Autocomplete {
         });
     }
 
+    /** @private */
     writeOptions(response) {
-        this.jQuery('.ac-options', `#${this.autocompleteId}`)
-            .empty()
-            .html(response);
+        this.clearOptions().html(response);
     }
 }
