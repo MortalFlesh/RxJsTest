771f71a98cbe7056b7bea11938f026f0f3a00074
Petr Chromec <mortal_flesh@seznam.cz>
2017-01-04 17:12:11 +0100

    new versions

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import Rx from "rx";
-
-const clickableObservable = Rx.Observable
-    .fromEvent(document, 'click');
-
-const clockObservable = clickableObservable
-    .map(click => Rx.Observable.interval(1000));
-
-clockObservable
-    .subscribe((clock) => clock.subscribe((x) => console.log(`tick ${x}`)));
+console.log('test');
